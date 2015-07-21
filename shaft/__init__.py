5c0ef9379f1391c976fd62521d8407d943c442ee
hammer <mike@themcclure.com>
2015-07-20 17:20:05 -0700

    doc setup

--- shaft/__init__.py
+++ shaft/__init__.py
@@ -1,5 +1,17 @@
-'''
-Module created by PyCharm
-'''
+"""Module to process and rank officials by experience.
+
+We have several parts:
+- load an official from an exported Excel file (old and new formats accepted)
+- load an official from live google sheets (not implemented)
+- summarise a list of officials from a given directory
+- standard options for processing
+- process tournament application sheets (not implemented)
+"""
 __author__ = 'hammer'
 
+#TODO: OO the whole lot
+#TODO: modularise the whole lot. options baby, options
+#TODO: standardise a few different weighting models
+
+if __name__ == '__main__':
+    print "Damn right"
