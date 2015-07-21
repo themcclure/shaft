3188d27d7f1f57d243f1112a126c3eb053efd97c
themcclure <mike@themcclure.com>
2015-07-20 19:42:42 -0700

    slow progress to equal functionality in an OO world

---
+++ shaft/Offical.py
@@ -0,0 +1,11 @@
+"""
+Module for the Offical class and utilities to house all the information in an official's officiating history document
+
+Theory is to keep all the raw data in summary form, and add the weighted values as well, that way either set of data can be used.
+Possibly ALL the weighting models can be applied on input and just used as needed?
+"""
+__author__ = 'hammer'
+
+class Official():
+    def __init__(self):
+        pass
